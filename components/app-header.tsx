1925273330c57987aa80e158f598a80bd85e62b1
mahesh <maheshmestri73@gmail.com>
2026-03-03 23:29:01 +0530

    updated to glass and normal icon

--- components/app-header.tsx
+++ components/app-header.tsx
@@ -1,7 +1,10 @@
 import { os } from "@/constant/os";
 import { useAppTheme } from "@/hooks/useTheme";
+import { isGlassEffectAPIAvailable } from "expo-glass-effect";
 import { useRouter } from "expo-router";
+import { useMemo } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Icon } from "react-native-paper";
 import { GlassIcon } from "./ui/glass-utility";
 
 interface apppheader { showBack?: boolean, title?: string, showRight?: boolean, rightComponent?: React.ReactNode }
@@ -9,13 +12,15 @@ interface apppheader { showBack?: boolean, title?: string, showRight?: boolean, 
 export function AppHeader({ showBack = true, title = "", showRight = false, rightComponent }: apppheader) {
     const theme = useAppTheme();
     const router = useRouter();
-
+    const glassEffect = useMemo(() => isGlassEffectAPIAvailable(), [])
     return (
         <View style={[styles.container, { backgroundColor: theme.color.background }]}>
             <Pressable onPress={() => router.back()}>
-                {showBack && <GlassIcon icon={os === 'android' ? 'arrow-left' : 'chevron-left'} color={theme.color.text} size={28} />}
+                {showBack && glassEffect ?
+                    <GlassIcon icon={os === 'android' ? 'arrow-left' : 'chevron-left'} color={theme.color.text} size={28} /> :
+                    <Icon source={os === 'android' ? 'arrow-left' : 'chevron-left'} color={theme.color.text} size={28} />}
             </Pressable>
-            <Text style={{ color: theme.color.text, fontSize: 16, fontWeight: 'bold' }}>{title}</Text>
+            <Text style={[styles.title, { color: theme.color.text }]}>{title}</Text>
             <View>
                 {showRight && rightComponent}
             </View>
@@ -29,5 +34,9 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between'
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold'
     }
 })
